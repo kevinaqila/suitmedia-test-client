540b223e0097b6203a085ff33b0ec6029f397885
KevinAqila <144351209+kevinaqila@users.noreply.github.com>
2025-07-17 18:13:37 +0700

    Initial commit

--- src/pages/IdeasPage.jsx
+++ src/pages/IdeasPage.jsx
@@ -1,39 +1,67 @@
+IdeasPage.jsx;
+
 import { useEffect, useState } from "react";
+
 import { useSearchParams } from "react-router-dom";
+
 import Card from "../components/Card";
+
 import FilterButton from "../components/FilterButton";
+
 import PageButton from "../components/PageButton";
+
 import Banner from "../components/Banner";
 
 export default function IdeasPage() {
   const [searchParams, setSearchParams] = useSearchParams();
+
   const [posts, setPosts] = useState([]);
+
   const [from, setFrom] = useState(null);
+
   const [to, setTo] = useState(null);
+
   const [total, setTotal] = useState(null);
+
   const [page, setPage] = useState(1);
+
   const [perPage, setPerPage] = useState(parseInt(searchParams.get("perPage")) || 10);
+
   const [sort, setSort] = useState(searchParams.get("sort") || "Newest");
+
   const [currentPage, setCurrentPage] = useState(null);
+
   const [totalPages, setTotalPages] = useState(null);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const sortParam = sort === "Newest" ? "-published_at" : "published_at";
+
       const params = new URLSearchParams({
         "page[number]": page,
+
         "page[size]": perPage,
+
         sort: sortParam,
       });
+
       try {
-        const res = await fetch(`/api/ideas?${params.toString()}`);
+        const res = await fetch(`http://localhost:3001/api/ideas?${params.toString()}`);
+
         const data = await res.json();
+
         setPosts(data.data);
+
         setTotal(data.meta.total);
+
         setFrom(data.meta.from);
+
         setTo(data.meta.to);
+
         setCurrentPage(data.meta.current_page);
+
         setTotalPages(data.meta.last_page);
       } catch (error) {
         console.log("Failed to fetch data", error);
@@ -47,16 +75,15 @@ export default function IdeasPage() {
 
   useEffect(() => {
     const newParams = { page, perPage, sort };
+
     setSearchParams(newParams, { replace: true });
   }, [page, perPage, sort, setSearchParams]);
 
   const handlePerPage = (e) => {
-    setPage(1);
     setPerPage(e.target.value);
   };
-  const handleSort = (e) => {
-    setPage(1);
 
+  const handleSort = (e) => {
     setSort(e.target.value);
   };
 
@@ -67,7 +94,6 @@ export default function IdeasPage() {
   return (
     <>
       <Banner title="Ideas" subtitle="Where all our great things begin" />
-
       <FilterButton
         from={from}
         to={to}
@@ -77,6 +103,7 @@ export default function IdeasPage() {
         valueSort={sort}
         onChangeSort={handleSort}
       />
+
       <div className="content">
         {loading ? (
           <p>Loading...</p>
